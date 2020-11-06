1f8c96a3a070370f288a5e1357420bbd8db741cc
weixing06 <weixings06@126.com>
2020-11-06 13:54:30 +0800

    cccc

--- stringbad.h
+++ stringbad.h
@@ -19,6 +19,28 @@
  *  默认的复制构造函数，逐个复制非静态成员（成员复制也称浅复制），复制的是成员的值
  *  赋值运算符，如果没有定义
  *  地址运算符，如果没有定义
+ *
+ *
+ *
+ *  对于指向的内存是由new分配的所有类成员，都应在类的析构函数中对其使用delete，该运算符将释放分配的内存
+ *  如果析构函数通过对指针类成员使用delete释放内存，则每个构造函数都应该使用new来初始化指针，或者将它设置为空指针
+ *  构造函数中要么使用new[] 要么使用new,而不能混用。
+ *  应定义一个分配内存的复制构造函数，这样程序将能够将类对象初始化为另一个类对象。这种构造函数的原型通常如下：
+ *      className(const className &)
+ *
+ *  应定义一个能重载赋值运算符的类成员函数，其函数定义如下
+ *      c_name & cname::operator=(cosnt c_name & cn)
+ *      {
+ *          if(this == &cn){
+ *           return *this;
+ *          }
+ *           delete [] c_pointer;
+ *           c_pointer = new type_name[size]
+ *           //then copy dota pointed to by cn.c_pointer to
+ *           //location pointer to by c_pointer
+ *           //
+ *          return * this;
+      }
  */
 class StringBad{
 private:
