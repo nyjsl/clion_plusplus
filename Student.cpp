6eed210ec89e41338076225e8a18159622c61862
weixing06 <weixings06@126.com>
2020-11-12 11:14:06 +0800

    privet ext

--- Student.cpp
+++ Student.cpp
@@ -10,10 +10,10 @@ using std::string;
 
 std::ostream &Student::arr_out(ostream &os) const {
     int i;
-    int lim = scores.size();
+    int lim = ArrayDb ::size();
     if (lim > 0) {
         for (i = 0; i < lim; i++) {
-            os << scores[i] << " ";
+            os << ArrayDb ::operator[](i) << " ";
             if (i % 5 == 4) {
                 os << endl;
             }
@@ -29,37 +29,37 @@ std::ostream &Student::arr_out(ostream &os) const {
 }
 
 double Student::Average() const {
-    if (scores.size() > 0) {
-        return scores.sum() / scores.size();
+    if (ArrayDb::size() > 0) {
+        return ArrayDb::sum() / ArrayDb::size();
     }
     return 0;
 }
 
 const std::string &Student::Name() const {
-    return name;
+    return (const string &) * this;
 }
 
 double &Student::operator[](int i) {
-    return scores[i];
+    return ArrayDb ::operator[](i);
 }
 
 double Student::operator[](int i) const {
-    return scores[i];
+    return ArrayDb::operator[](i);
 }
 
 std::ostream &operator<<(ostream &os, const Student &student) {
-    os << "Scores for "<< student.name << ":\n";
+    os << "Scores for "<< (const string&) student << ":\n";
     student.arr_out(os);
     return os;
 }
 
 std::istream &operator>>(istream &is, Student &stu) {
-    is >> stu.name;
+    is >> (string&) stu;
     return is;
 }
 
 std::istream & getline(istream &is, Student &stu) {
-    getline(is, stu.name);
+    getline(is, (string&) stu);
     return is;
 }
 
--- Student.h
+++ Student.h
@@ -11,22 +11,30 @@
 /**
  * 初始化顺序
  *  ，当初始化列表包含多个项目时，这些项目被初始化的顺序为它们被声明的顺序，而不是它们在初始化列表中的顺序。
+ *
+ *  私有多继承,注释掉的代码为不是使用继承是的版本
  */
-class Student {
+class Student :private std::string,private std::valarray<double>{
 private:
     typedef std::valarray<double> ArrayDb;
-    std::string name;
-    ArrayDb scores;
+//    std::string name;
+//    ArrayDb scores;
     std::ostream &arr_out(std::ostream & os) const;
 public:
-    Student():name("Null Student"),scores(){}
-    explicit Student(const std::string & s):name(s),scores(){}
-    explicit Student (int n):name("Nully"),scores(n){}
 
+//    Student():name("Null Student"),scores(){}
+//    explicit Student(const std::string & s):name(s),scores(){}
+//    explicit Student (int n):name("Nully"),scores(n){}
+//    Student(const std::string & s,int n):name(s),scores(n){}
+//    Student(const std::string & s,const ArrayDb & a):name(s),scores(a){}
+//    Student(const char*  str,const double *pd,int n):name(str),scores(pd,n){}
 
-    Student(const std::string & s,int n):name(s),scores(n){}
-    Student(const std::string & s,const ArrayDb & a):name(s),scores(a){}
-    Student(const char*  str,const double *pd,int n):name(str),scores(pd,n){}
+    Student():std::string("Null Student"),ArrayDb(){}
+    explicit Student(const std::string & s):std::string(s),ArrayDb(){}
+    explicit Student (int n):std::string("Nully"),ArrayDb(n){}
+    Student(const std::string & s,int n):std::string(s),ArrayDb(n){}
+    Student(const std::string & s,const ArrayDb & a):std::string(s),ArrayDb(a){}
+    Student(const char*  str,const double *pd,int n):std::string(str),ArrayDb(pd,n){}
 
     ~Student(){}
 
